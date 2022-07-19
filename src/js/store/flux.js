34a4b0c1015e447a792cc6bdea9d3a625eb85775
FelipeV1993 <f.vasquezossa@gmail.com>
2022-07-19 09:34:08 -0400

    start

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,102 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
+  return {
+    store: {
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+      people: null,
+	  planets: null,
+	  vehicles: null,
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadSomeData: () => {
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        //reset the global store
+        setStore({ demo: demo });
+      },
+      getPeople: (url) => {
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+            console.log(response);
+            return response.json();
+          })
+          .then((data) => {
+            setStore({ people: data });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      getPlanets: (url) => {
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+            console.log(response);
+            return response.json();
+          })
+          .then((data) => {
+            setStore({ planets: data });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      getVehicles: (url) => {
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+            console.log(response);
+            return response.json();
+          })
+          .then((data) => {
+            setStore({ vehicles: data });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,103 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Card } from "../component/card";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [people,setPeople]=useState(null)
+
+	// const getPeople = () => {
+	// 	fetch('https://swapi.dev/api/people/', {
+	// 		method: 'GET',
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+	// 		}
+	// 	})
+	// 	.then((response) => {
+	// 	console.log(response);
+	// 	return response.json();
+	// 	})
+	// 	.then((data) => {
+	// 	setPeople(data);
+
+	// 	})
+	// 	.catch((error) => {
+	// 	console.log(error);
+	// 	});
+	// };
+	// useEffect(() => {
+	// 	getPeople();
+	//   }, []);
+	  const testPeople =()=>{
+		console.log(store.people.results)
+	  }
+	  const testPlanets =()=>{
+		console.log(store.planets.results)
+	  }
+	  const testVehicles =()=>{
+		console.log(store.vehicles.results)
+	  }
+
+return(<>
+<div className="row row-cols-3 mt-2  g-4">
+  <div className="col">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          This is a longer card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          This is a longer card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+  </div><div className="col">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          This is a longer card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+  </div><div className="col">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          This is a longer card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+  </div>
+  
+  
+</div>
+
+
+
+<button onClick={testPeople}>test people</button>
+<button onClick={testPlanets}>test planets</button>
+<button onClick={testVehicles}>test vehicles</button>
+</>
+)
+	
+};
